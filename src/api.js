f617cbc8a8d0f2b6dc1f91da716f8cc953e6cfe9
Agoziemax <agoziemax@yahoo.com>
2023-05-11 22:02:14 +0100

    first commit

---
+++ src/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+
+const searchImages = async (term) =>{
+  const response = await axios.get('https://api.unsplash.com/search/photos', {
+    headers: {
+      Authorization: 'Client-ID xt9NTj_xPDgeck6g3J8In6ok1EL7Y0-CTI6E60R0kcQ',
+    },
+    params: {
+      query: term,
+    },
+  });
+  
+  return response.data.results;
+}
+
+export default searchImages
---
+++ src/components/SearchBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useState } from 'react';
+import './SearchBar.css'
+
+function SearchBar({ onSubmit }) {
+  const [input, setInput] = useState('');
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(input);
+  };
+
+  return (
+    <div className='search-bar'>
+      <form onSubmit={handleFormSubmit}>
+        <label>Enter keyword to search for</label>
+        <input type='text' value={input} onChange={handleChange} placeholder='Search...' />
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
